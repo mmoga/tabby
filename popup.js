974b0171097e97adb653e37febafd5feafe6c06f
mmoga <matt.moga@gmail.com>
2018-08-28 18:19:42 -0400

    Finished the Chrome tutorial. Feel free to start changing things~

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
-require('dotenv').config()
-const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN } = process.env;
+// require('dotenv').config()
+// const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN } = process.env;
 
-// Initialize Firebase
-var config = {
-apiKey: FIREBASE_API_KEY,
-authDomain: FIREBASE_AUTH_DOMAIN,
-databaseURL: "https://tabby-3ece8.firebaseio.com",
-projectId: "tabby-3ece8",
-storageBucket: "tabby-3ece8.appspot.com",
-messagingSenderId: "142571884018"
-};
-firebase.initializeApp(config);
+// // Initialize Firebase
+// var config = {
+// apiKey: FIREBASE_API_KEY,
+// authDomain: FIREBASE_AUTH_DOMAIN,
+// databaseURL: "https://tabby-3ece8.firebaseio.com",
+// projectId: "tabby-3ece8",
+// storageBucket: "tabby-3ece8.appspot.com",
+// messagingSenderId: "142571884018"
+// };
+// firebase.initializeApp(config);
 
 let changeColor = document.getElementById('changeColor');
 
@@ -18,5 +18,14 @@ let changeColor = document.getElementById('changeColor');
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
   });
+
+  changeColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
 
   
