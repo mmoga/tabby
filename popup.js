fab8e8bfa2601cc5af87cf867591428573e1e0c5
mmoga <matt.moga@gmail.com>
2018-08-28 19:41:29 -0400

    Fixed typo :-|

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-slet changeColor = document.getElementById('changeColor');
+let changeColor = document.getElementById('changeColor');
 
   chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
