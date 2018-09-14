833db1b51f36186d8013016ed2a07fe477da3c2e
mmoga <matt.moga@gmail.com>
2018-09-14 17:17:40 -0400

    Used create-react-app and removed the initial unneccessary

---
+++ tabby/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tabby</h1>
+        </header>
+        <p className="App-intro">
+          Share tabs with Chris — or Matthew — depending on who's reading this.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
